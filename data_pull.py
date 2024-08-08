83024f85cb0dc11fba348bd4e3ac26351a028fd3
Yanarp Madnug <pranaygundam00@gmail.com>
2024-08-08 08:51:03 -0400

    new api

--- data_pull.py
+++ data_pull.py
@@ -1,7 +1,8 @@
-from fredapi import Fred
+import pyfredapi as pf
 from dotenv import load_dotenv
 import os
 from datetime import date
+import random
 
 FRED_CATEGORIES = ["Money, Banking, & Finance", "Population, Employment, & Labor Markets", "National Accounts", 
                    "Production & Business Activity", "Prices", "U.S. Regional Data", "International Data",
@@ -9,22 +10,45 @@ FRED_CATEGORIES = ["Money, Banking, & Finance", "Population, Employment, & Labor
 
 FRED_CATEGORY_IDS = [32991, 10, 32992, 1, 32455, 3008, 32263, 33060]
 
-def load_fred_obj():
-    # Loading the .env file
-    load_dotenv()
+class Fred:
+    def __init__(self, api_key = None):
+        if api_key is None:
+            load_dotenv()
 
-    # Store credentials from .env file
-    FRED_API_KEY = os.getenv('FRED_API_KEY')
+            # Store credentials from .env file
+            FRED_API_KEY = os.getenv('FRED_API_KEY')
+            self.api_key = FRED_API_KEY
+        else:
+            self.api_key = api_key
 
-    fred = Fred(api_key=FRED_API_KEY)
+    def get_category(self, category_id):
+        return pf.get_category(category_id=category_id, api_key=self.api_key)
 
-    return fred
+    def get_category_children(self, category_id):
+        return pf.get_category_children(category_id=category_id, api_key=self.api_key)
 
+    def choose_random_category(self, category_id = 0):
+        children = self.get_category_children(category_id)["categories"]
+        id = None
+        name = None
+        while not children:
+            random_child = random.choice(children)
+            id = random_child["id"]
+            name = random_child["name"]
+            children = self.get_category_children(random_child["id"])["categories"]
+        return id, name
 
-def update_series_list():
-    fred = load_fred_obj()
+    def get_category_series(self, category_id):
+        return pf.get_category_series(category_id=category_id, api_key=self.api_key)
 
+    def choose_random_series(self, category_id = 0):
+        category_id, name = self.choose_random_category(category_id)
+        series = self.get_category_series(category_id)["series"]
 
+    def get_series(self, series_id):
+        return pf.get_series(series_id="GDP", api_key=self.api_key)
+
+def update_series_list(fred):
     # Iterate through all the categories and write a csv file with
     # categories and series in a tuple
     for category in FRED_CATEGORIES:
@@ -36,22 +60,17 @@ def update_series_list():
     series_list.to_csv('series_list.csv')
 
 
-def pull_random_fred_series():
-
-    fred = load_fred_obj()
-
+def pull_random_fred_series(fred):
+    pass
     #df1 = fred.get_series('SP500')
     #df2 = fred.get_series('SP500')
 
 
 
 # Testing
-fred = load_fred_obj()
-
-for i in FRED_CATEGORY_IDS:
-    try:
-        print(fred.search_by_category(i, order_by='series_id').head())
-    except:
-        print(f"Error with category {i}")   
-
-# Alternative method is to just try random numbers until one works!
+fred = Fred()
+FRED_API_KEY = os.getenv('FRED_API_KEY')
+hello = pf.get_category_children(34009, FRED_API_KEY)
+series = pf.get_category_series(34009, FRED_API_KEY)
+print(hello)
+print(series.keys())
